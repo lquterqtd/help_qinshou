f4ab8f15cd7dd9d9c5e007bd8972841da93e3da0
lquterqtd <378203086@qq.com>
2012-11-04 05:26:34 +0800

    解决了No module named _fontdata_enc_winansi打包错 解决了编码问题 调整了表格后三列内容的对齐方式
    Signed-off-by: lquterqtd <378203086@qq.com>

--- GenPDF.py
+++ GenPDF.py
@@ -1,6 +1,29 @@
 __author__ = 'Administrator'
 #coding:cp936
 
+#for setup error:No module named _fontdata_enc_winansi
+from reportlab.pdfbase import _fontdata_enc_winansi
+from reportlab.pdfbase import _fontdata_enc_macroman
+from reportlab.pdfbase import _fontdata_enc_standard
+from reportlab.pdfbase import _fontdata_enc_symbol
+from reportlab.pdfbase import _fontdata_enc_zapfdingbats
+from reportlab.pdfbase import _fontdata_enc_pdfdoc
+from reportlab.pdfbase import _fontdata_enc_macexpert
+from reportlab.pdfbase import _fontdata_widths_courier
+from reportlab.pdfbase import _fontdata_widths_courierbold
+from reportlab.pdfbase import _fontdata_widths_courieroblique
+from reportlab.pdfbase import _fontdata_widths_courierboldoblique
+from reportlab.pdfbase import _fontdata_widths_helvetica
+from reportlab.pdfbase import _fontdata_widths_helveticabold
+from reportlab.pdfbase import _fontdata_widths_helveticaoblique
+from reportlab.pdfbase import _fontdata_widths_helveticaboldoblique
+from reportlab.pdfbase import _fontdata_widths_timesroman
+from reportlab.pdfbase import _fontdata_widths_timesbold
+from reportlab.pdfbase import _fontdata_widths_timesitalic
+from reportlab.pdfbase import _fontdata_widths_timesbolditalic
+from reportlab.pdfbase import _fontdata_widths_symbol
+from reportlab.pdfbase import _fontdata_widths_zapfdingbats
+
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen import canvas
@@ -12,15 +35,18 @@ from reportlab.lib.units import cm
 MyFont = 'simsun.ttc'
 MyFontName = 'song'
 
+def my_convert(str):
+    return str.decode('gb2312').encode('utf-8')
+
 def GenPDF(filename, classified_product_list, Specified_Order):
     pdfmetrics.registerFont(TTFont(MyFontName, MyFont))
     elements = []
     styles = getSampleStyleSheet()
     doc = SimpleDocTemplate(filename)
     styles['Title'].fontName = MyFontName
-    elements.append(Paragraph("清单", styles['Title']))
+    elements.append(Paragraph(my_convert("清单"), styles['Title']))
     data = [
-        ['货号', '商品名称', '颜色', '尺码', '数量']
+        [my_convert('货号'), my_convert('商品名称'), my_convert('颜色'), my_convert('尺码'), my_convert('数量')]
     ]
 
     for prefix in Specified_Order:
@@ -43,7 +69,8 @@ def GenPDF(filename, classified_product_list, Specified_Order):
     table.setStyle(TableStyle([
         ('FONT', (0,0), (-1,-1), MyFontName),
         ('ALIGN', (0,0), (4,0), 'CENTER'),
-        ('ALIGN', (0,1), (-1,-1), 'LEFT'),
+        ('ALIGN', (0,1), (1,-1), 'LEFT'),
+        ('ALIGN', (2,1), (4,-1), 'CENTER'),
         ('GRID', (0,0), (-1,-1), 1, colors.black),
     ]))
     elements.append(table)
